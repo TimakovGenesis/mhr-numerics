fe12b98885840de029d6d6b026d2feb0c7dad88e
TimakovGenesis <profipublic@gmail.com>
2026-03-29 21:45:30 +0300

    Create __init__.py

---
+++ Part_I/__init__.py
@@ -0,0 +1,8 @@
+"""
+mhr_numerics.Part_I
+===================
+Variational MHR solvers and foundations of arithmetic spectroscopy.
+
+Modules:
+    variational     - MHR variational solver (Gaussian ansatz, L-BFGS-B)
+"""
